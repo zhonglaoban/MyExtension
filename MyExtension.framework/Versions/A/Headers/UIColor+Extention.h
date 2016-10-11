1f7928d677abc6b4b2424964c7dc833a268e174b
钟凡 <1049056949@>
2016-10-11 16:08:41 +0800

    add pod files

---
+++ MyExtension.framework/Versions/A/Headers/UIColor+Extention.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extention.h
+//  MyRill
+//
+//  Created by 钟凡 on 16/2/1.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Extention)
+
++(UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpa;
+
+@end
